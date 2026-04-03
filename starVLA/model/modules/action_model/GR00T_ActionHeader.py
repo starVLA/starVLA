9e95f9cb4dbb5262510373da13251797b953e4a6
yangyi <yangyi@yangyideMacBook-Pro.local>
2026-04-03 21:01:00 +0800

    [fix] resolve potential negative sampling in sample_time

--- starVLA/model/modules/action_model/GR00T_ActionHeader.py
+++ starVLA/model/modules/action_model/GR00T_ActionHeader.py
@@ -261,7 +261,7 @@ class FlowmatchingActionHead(nn.Module):
 
     def sample_time(self, batch_size, device, dtype):
         sample = self.beta_dist.sample([batch_size]).to(device, dtype=dtype).clamp(max=self.config.noise_s)
-        return (self.config.noise_s - sample) / self.config.noise_s
+        return self.config.noise_s * (1 - sample)
 
     def prepare_input(self, batch: dict) -> BatchFeature:
         return BatchFeature(data=batch)
